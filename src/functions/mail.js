b40295d4b11567a591cc71bfbd929aba7b07d32f
Josef Lekardal <josef@workbuster.se>
2020-05-04 10:58:25 +0200

    add googlesheet support

--- src/functions/mail.js
+++ src/functions/mail.js
@@ -1,12 +1,30 @@
-const { MAILGUN_KEY, MAILGUN_DOMAIN, MAILGUN_ENDPOINT } = process.env
-const descriptions = require('./descriptions')
+const {
+  MAILGUN_KEY, MAILGUN_DOMAIN, MAILGUN_ENDPOINT,
+  GOOGLE_SHEET_ID, GOOGLE_PROJECT_ID, GOOGLE_PRIVATE_KEY_ID, GOOGLE_PRIVATE_KEY,
+  GOOGLE_CLIENT_EMAIL, GOOGLE_CLIENT_X509_CERT_URL, GOOGLE_CLIENT_ID
+} = process.env
+
+const { GoogleSpreadsheet } = require('google-spreadsheet')
 const mailgun = require('mailgun-js')({
   apiKey: MAILGUN_KEY,
   domain: MAILGUN_DOMAIN,
   host: MAILGUN_ENDPOINT
 })
 
-console.log({ MAILGUN_KEY, MAILGUN_DOMAIN, MAILGUN_ENDPOINT })
+const descriptions = require('./descriptions')
+
+const GOOGLE_AUTH = {
+  type: 'service_account',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  project_id: GOOGLE_PROJECT_ID,
+  private_key_id: GOOGLE_PRIVATE_KEY_ID,
+  private_key: GOOGLE_PRIVATE_KEY,
+  client_email: GOOGLE_CLIENT_EMAIL,
+  client_x509_cert_url: GOOGLE_CLIENT_X509_CERT_URL,
+  client_id: GOOGLE_CLIENT_ID
+}
 
 const mailgunPromise = (data) => {
   return new Promise((res, rej) => {
@@ -19,8 +37,24 @@ const mailgunPromise = (data) => {
   })
 }
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event) => {
   const { topGifts, email, campus } = JSON.parse(event.body)
+
+  try {
+    const doc = new GoogleSpreadsheet(GOOGLE_SHEET_ID)
+    await doc.useServiceAccountAuth(GOOGLE_AUTH)
+
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[0]
+    const gifts = topGifts.reduce((acc, cur) => {
+      acc[cur] = true
+      return acc
+    }, {})
+    await sheet.addRow({ email, ...gifts, campus })
+  } catch (error) {
+    console.error('SOMETHING IS WRONG!')
+    console.error(error)
+  }
 
   const giftMarkup = topGifts.map(gift => descriptions[gift]).join('')
 
@@ -50,20 +84,23 @@ exports.handler = async (event, context, callback) => {
 
   try {
     const body = await mailgunPromise(data)
-    callback(null, {
+
+    return {
       statusCode: 200,
       body: JSON.stringify(body),
       headers: {
         'Content-Type': 'text/html'
       }
-    })
+    }
   } catch (error) {
     console.log('There was an error sending the email')
     console.log(error)
   
-    callback(null, {
+    return {
       statusCode: 400,
-      body: JSON.stringify(error)
-    })
+      body: JSON.stringify({
+        msg: 'There was an error sending you email'
+      })
+    }
   }
 }
