ad9d3af0f8c5d4115746d283192a79b305381f37
Josef Lekardal <josef@workbuster.se>
2020-05-04 22:46:36 +0200

    remove not used file

--- formatSpreadSheetData.js
+++
@@ -1,8 +0,0 @@
-module.exports = function formatSpreadSheetData (data) {
-  return data.split('\n')
-    .reduce((acc, cur) => {
-      let [question, type] = cur.split('\t')
-      question = question.replace(/\d+\.\s?/, '')
-      return acc.concat({ question, type })
-    }, [])
-}
